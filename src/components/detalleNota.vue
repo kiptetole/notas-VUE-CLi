6d9f67d902056e40fe3999d821bec0d8ee8623a8
kiptetole <jonomi97@gmail.com>
2020-03-02 17:23:35 +0100

    Prioridad en las Notas

--- src/components/detalleNota.vue
+++ src/components/detalleNota.vue
@@ -3,19 +3,24 @@
   <div class="conatiner-fluid">
     <div class="row">
       <div class="col-1">
-        <img class="completo" v-if="hecho == true" src="../assets/check2.png" @click="hacer">
-        <img class="incomp" v-else src="../assets/check1.png" @click="hacer">
+        <img class="completo icon" v-if="hecho == true" src="../assets/check2.png" @click="hacer">
+        <img class="incomp icon" v-else src="../assets/check1.png" @click="hacer">
       </div>
       <div class="col-10 d-flex justify-content-start" v-bind:class="{checked: hecho}">
         <p class="d-inline display-4 ">{{msg}}</p>
       </div>
       <div class="col-1">
-        <img class="borrar" src="../assets/borrar.png" @click="borrar">
+        <img class="borrar icon" src="../assets/borrar.png" @click="borrar">
       </div>
     </div>
     <div class="row">
       <div class="col-3 d-flex justify-content-start">
-        <p class="d-inline">{{prioridad}}</p>
+				<button v-if="prioridad=='0'" @click="prBaja" class="pr baja col-xs-12">Baja</button>
+        <button v-else @click="prBaja" class="pr disable col-xs-12">Baja</button>
+        <button v-if="prioridad=='1'" @click="prNormal" class="pr normal col-xs-12">Normal</button>
+        <button v-else @click="prNormal" class="pr disable col-xs-12">Normal</button>
+        <button v-if="prioridad=='2'" @click="prAlta" class="pr alta col-xs-12">Alta</button>
+        <button v-else @click="prAlta" class="pr disable col-xs-12">Alta</button>
       </div>
       <div class="col-5 d-flex justify-content-start">
         <p class="d-inline">{{fecha}}</p>
@@ -43,7 +48,16 @@
       },
       borrar: function() {
         this.$emit("borrar", this.fecha)
-      }
+      },
+      prBaja: function(){
+        this.$emit("prBaja", this.fecha, 0)
+      },
+      prNormal: function(){
+        this.$emit("prNormal", this.fecha, 1)
+      },
+      prAlta: function(){
+        this.$emit("prAlta", this.fecha, 2)
+      },
     },
     computed: {
 
@@ -63,8 +77,47 @@
     text-decoration: line-through;
 }
 
-img{
+.icon{
   height: 3rem;
   width: 3rem;
 }
+.pr.baja,.pr.normal,.pr.alta{
+		-moz-border-radius:6px;
+		-webkit-border-radius:6px;
+		border-radius:6px;
+		display:inline-block;
+		cursor:pointer;
+		color:black;
+		font-size:10px;
+		font-weight:bold;
+		padding:6px 15px;
+		text-decoration:none;
+	}
+	.disable{
+		background-color:#dadada;
+		-moz-border-radius:6px;
+		-webkit-border-radius:6px;
+		border-radius:6px;
+		border:1px solid #464545;
+		display:inline-block;
+		cursor:pointer;
+		color:white;
+		font-family:Arial;
+		font-size:10px;
+		font-weight:bold;
+		padding:6px 15px;
+		text-decoration:none;
+	}
+	.pr.baja{
+		background-color:#17a2b8;
+		border:1px solid #17a2b8;
+	}
+	.pr.normal{
+		background-color:#375a7f;
+		border:1px solid #375a7f;
+	}
+	.pr.pr.alta{
+		background-color:#e74d3d;
+		border:1px solid #e74d3d;
+  }
 </style>
--- src/components/lista.vue
+++ src/components/lista.vue
@@ -7,12 +7,13 @@
     <hr/>
     <div>
       hay un total de {{todo.length}} notas y hay {{tareasHechas()}} tareas completadas.
-      <button type="button" class="btn btn-success" @click="borrarTareasCompletadas()">Borrar las tareas completadas</button>
+      <button type="button" class="btn btn-success" @click="borrarTareasCompletadas">Borrar las tareas completadas</button>
     </div>
     <br/>
     <ul class="list-group">
       <li v-for="(nota,index) in todo" v-bind:key="index" class="list-group-item">
-        <detalleNota v-bind:msg="nota.msg" v-bind:prioridad="nota.prioridad" v-bind:fecha="nota.fecha" v-bind:hecho="nota.hecho" @hecho="hechono" @borrar="borrarNota"></detalleNota>
+        <detalleNota v-bind:msg="nota.msg" v-bind:prioridad="nota.prioridad" v-bind:fecha="nota.fecha" v-bind:hecho="nota.hecho" @hecho="hechono" @borrar="borrarNota" 
+        @prBaja="cambiaPrioridad" @prNormal="cambiaPrioridad" @prAlta="cambiaPrioridad"></detalleNota>
       </li>
     </ul>
   </div>
@@ -41,6 +42,20 @@ import entradaNota from './entradaNota';
       }
     },
     methods: {
+      guardarInformacion: function(){
+          const infoJSON = JSON.stringify(this.todo)
+          localStorage.setItem('todo', infoJSON)
+        },
+      indexar: function (msg, prioridad, fecha) {
+        var nota={
+          msg: msg,
+          prioridad: prioridad,
+          fecha: fecha,
+          hecho: false
+        }
+        this.todo.push(nota);
+        this.guardarInformacion();
+      },
       hechono: function(hecho,fecha){
         var nota = 0
         for (let i=0; i<this.todo.length; i++){
@@ -63,16 +78,6 @@ import entradaNota from './entradaNota';
         }
         this.guardarInformacion()
       },
-      indexar: function (msg, prioridad, fecha) {
-        var nota={
-          msg: msg,
-          prioridad: prioridad,
-          fecha: fecha,
-          hecho: false
-        }
-        this.todo.push(nota);
-        this.guardarInformacion();
-      },
       tareasHechas: function () {
         var result=0;
         for(let i=0;i<this.todo.length;i++){
@@ -83,17 +88,22 @@ import entradaNota from './entradaNota';
         return result;
       },
       borrarTareasCompletadas: function(){
-        for (let i; i<this.todo.length; i++){
+        for(let i=0; i<this.todo.length; i++){
           if (this.todo[i].hecho){
-            console.log(i);
+            this.todo.splice( i, 1 );
           }
         }
-        this.guardarInformacion()
       },
-      guardarInformacion: function(){
-          const infoJSON = JSON.stringify(this.todo)
-          localStorage.setItem('todo', infoJSON)
-        },
+      cambiaPrioridad: function (fecha,prioridad) {
+        var nota = 0
+        for (let i=0; i<this.todo.length; i++){
+          if (this.todo[i].fecha == fecha){
+            nota = i;
+          }
+        }
+        this.todo[nota].prioridad = prioridad
+        this.guardarInformacion()
+      }  
     },
     computed: {
 
@@ -104,4 +114,3 @@ import entradaNota from './entradaNota';
   }
 }
 </script>
-
